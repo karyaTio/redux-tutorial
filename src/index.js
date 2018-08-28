cd888a0d17b85f34105107b449ae296807886a86
Tio <karya.tiosaputra@gmail.com>
2018-08-28 21:54:32 +0700

    First Push

---
+++ src/index.js
@@ -0,0 +1,27 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import registerServiceWorker from "./registerServiceWorker";
+
+import { combineReducers, createStore } from "redux";
+
+function productsReducer(state = [], action) {
+  return state;
+}
+
+function userReducer(state = "", action) {
+  return state;
+}
+
+const allReducers = combineReducers({
+  product: productsReducer,
+  user: userReducer
+});
+
+const store = createStore(allReducers);
+
+console.log(store.getState());
+
+ReactDOM.render(<App />, document.getElementById("root"));
+registerServiceWorker();
